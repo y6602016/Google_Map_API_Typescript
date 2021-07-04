c73f412da88c49cc9239d675bab92fb5bfcdb0e2
mike <y6602016@gmail.com>
2021-07-04 23:40:26 +0800

    integrate Google Maps API

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google_Map_API_Typescript
+# Google_Map_API_Typescript
+# Google_Map_API_Typescript
---
+++ src/Company.ts
@@ -0,0 +1,25 @@
+import faker from "faker";
+import { Mappable } from "./CustomMap";
+
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrase: string;
+  color: string = "blue";
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `Company name: ${this.companyName}`;
+  }
+}
